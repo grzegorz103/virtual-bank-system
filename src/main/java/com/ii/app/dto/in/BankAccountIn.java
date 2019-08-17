434e42e759c034cba9df5a85ebc2bf15ae8b8406
Grzesiek <grzesiek555_12@outlook.com>
2019-08-17 23:22:34 +0200

    Add mapstruct

--- src/main/java/com/ii/app/dto/in/BankAccountIn.java
+++ src/main/java/com/ii/app/dto/in/BankAccountIn.java
@@ -1,6 +1,20 @@
 package com.ii.app.dto.in;
 
+import com.ii.app.models.Saldo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class BankAccountIn
 {
+        private String number;
 
+        private Set<Saldo> saldos;
+
+        private boolean multiCurrency;
 }
--- src/main/java/com/ii/app/services/BankAccountServiceImpl.java
+++ src/main/java/com/ii/app/services/BankAccountServiceImpl.java
@@ -2,22 +2,60 @@ package com.ii.app.services;
 
 import com.ii.app.dto.in.BankAccountIn;
 import com.ii.app.dto.out.BankAccountOut;
+import com.ii.app.mappers.BankAccountMapper;
+import com.ii.app.models.BankAccount;
+import com.ii.app.repositories.BankAccountRepository;
 import com.ii.app.services.interfaces.BankAccountService;
+import com.ii.app.utils.Constants;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@Transactional
 public class BankAccountServiceImpl implements BankAccountService
 {
-        @Override
-        public BankAccountOut create ( BankAccountIn bankAccountIn )
+        private final BankAccountMapper bankAccountMapper;
+
+        private final BankAccountRepository bankAccountRepository;
+
+        private final Constants constants;
+
+        @Autowired
+        public BankAccountServiceImpl ( BankAccountMapper bankAccountMapper,
+                                        BankAccountRepository bankAccountRepository,
+                                        Constants constants )
         {
-                return null;
+                this.bankAccountMapper = bankAccountMapper;
+                this.bankAccountRepository = bankAccountRepository;
+                this.constants = constants;
+        }
+
+        @Override
+        public BankAccountOut create ( @NotNull BankAccountIn bankAccountIn )
+        {
+                BankAccount bankAccount = bankAccountMapper.DTOtoEntity( bankAccountIn );
+                bankAccount.setSaldos( new HashSet<>() );
+                bankAccount.setNumber( RandomStringUtils.randomNumeric( constants.BANK_ACCOUNT_NUMBER_LENGTH ) );
+
+                return bankAccountMapper.entityToDTO(
+                        bankAccountRepository.save( bankAccount )
+                );
         }
 
         @Override
         public List<BankAccountOut> findAll ()
         {
-                return null;
+                return bankAccountRepository.findAll()
+                        .stream()
+                        .map( bankAccountMapper::entityToDTO )
+                        .collect( Collectors.toList() );
         }
 
         @Override
