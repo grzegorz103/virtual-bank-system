d55de2d144f853ef5d4f6ee21ccd3d70267a29b5
Grzesiek <grzesiek555_12@outlook.com>
2019-09-05 15:22:31 +0200

    Add exchange currency component to routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { BankAccountListComponent } from './main/views/bank-account-list/bank-account-list.component';
 import { TransactionComponent } from './main/views/transaction/transaction.component';
 import { TransactionMultiCurrencyComponent } from './main/views/transaction-multi-currency/transaction-multi-currency.component';
+import { ExchangeCurrencyComponent } from './main/views/exchange-currency/exchange-currency.component';
 
 
 const routes: Routes = [
   {path: 'bankAccounts', component: BankAccountListComponent},
   {path: 'transactions/create', component: TransactionComponent},
-  {path: 'transactions/create/multicurrency', component: TransactionMultiCurrencyComponent}
+  {path: 'transactions/create/multicurrency', component: TransactionMultiCurrencyComponent},
+  {path: 'exchange', component: ExchangeCurrencyComponent}
 ];
 
 @NgModule({
