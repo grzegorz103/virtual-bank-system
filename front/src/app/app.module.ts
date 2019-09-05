78071cb43e20487ec4d8fed9bc6117d398e6f1cb
Grzesiek <grzesiek555_12@outlook.com>
2019-09-05 22:53:45 +0200

    Edit transaction page

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -15,7 +15,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatStepperModule } from '@angular/material/stepper';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule, MatOptionModule, MatSelectModule } from '@angular/material';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatCardModule, MatOptionModule, MatSelectModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,6 +39,7 @@ import { MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule, M
     MatInputModule,
     MatOptionModule,
     MatSelectModule,
+    MatCardModule
   ],
   providers: [BankAccountService],
   bootstrap: [AppComponent]
