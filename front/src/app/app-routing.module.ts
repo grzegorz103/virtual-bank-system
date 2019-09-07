9bcf4fcb2a1681eef4939ed0f6b9370f3ecd89ed
Grzesiek <grzesiek555_12@outlook.com>
2019-09-07 18:22:49 +0200

    Add chart to bank acc details page

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { BankAccountListComponent } from './main/views/bank-account-list/bank-ac
 import { TransactionComponent } from './main/views/transaction/transaction.component';
 import { TransactionMultiCurrencyComponent } from './main/views/transaction-multi-currency/transaction-multi-currency.component';
 import { ExchangeCurrencyComponent } from './main/views/exchange-currency/exchange-currency.component';
+import { BankAccountDetailsComponent } from './main/views/bank-account-details/bank-account-details.component';
 
 
 const routes: Routes = [
   {path: 'bankAccounts', component: BankAccountListComponent},
   {path: 'transactions/create', component: TransactionComponent},
   {path: 'transactions/create/multicurrency', component: TransactionMultiCurrencyComponent},
+  {path: 'bankAccounts/:id/details', component: BankAccountDetailsComponent},
   {path: 'exchange', component: ExchangeCurrencyComponent}
 ];
 
---
+++ front/src/app/main/views/bank-account-details/bank-account-details.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-account-details',
+  templateUrl: './bank-account-details.component.html',
+  styleUrls: ['./bank-account-details.component.scss']
+})
+export class BankAccountDetailsComponent implements OnInit {
+
+  public chartDatasets: Array<any> = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
+  ];
+
+  public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: 'rgba(105, 0, 132, .2)',
+      borderColor: 'rgba(200, 99, 132, .7)',
+      borderWidth: 2,
+    },
+    {
+      backgroundColor: 'rgba(0, 137, 132, .2)',
+      borderColor: 'rgba(0, 10, 130, .7)',
+      borderWidth: 2,
+    }
+  ];
+  public chartType: string = 'line';
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+}
