16e1fda40affa2a17643026b625dcdb48be96782
Grzesiek <grzesiek555_12@outlook.com>
2019-09-25 18:01:55 +0200

    Fix currency type on bank acc list page

--- front/src/app/main/models/transaction.ts
+++ front/src/app/main/models/transaction.ts
@@ -1,6 +1,6 @@
 import { TransactionHistory } from './transaction-history';
 
-export class Transaction implements TransactionHistory {
+export class Transaction{
     id: string;
     sourceAccountNumber: string;
     sourceCurrency: string;
@@ -9,18 +9,4 @@ export class Transaction implements TransactionHistory {
     balance: number;
     title: string;
     date: Date;
-    type: string;
-
-
-    constructor(transaction?: Transaction, type?) {
-        this.id = transaction.id;
-        this.sourceAccountNumber = transaction.sourceAccountNumber;
-        this.sourceCurrency = transaction.sourceCurrency;
-        this.destinedAccountNumber = transaction.destinedAccountNumber;
-        this.balance = transaction.balance;
-        this.destinedCurrency = transaction.destinedCurrency;
-        this.title = transaction.title;
-        this.date = transaction.date;
-        this.type = type;
-    }
 }
