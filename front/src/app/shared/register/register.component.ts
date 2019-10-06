b012bc61fa162f0a8f5627c907d068e57f0f4f59
Grzesiek <grzesiek555_12@outlook.com>
2019-10-06 15:27:53 +0200

    Update register form

--- front/src/app/shared/register/register.component.ts
+++ front/src/app/shared/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  form: FormGroup;
+  
   constructor() { }
 
   ngOnInit() {
