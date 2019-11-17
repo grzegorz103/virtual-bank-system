5e8db04d588b4745ae4603ce633ade9638289c04
Grzesiek <grzesiek555_12@outlook.com>
2019-11-17 16:15:43 +0100

    Update date formats

---
+++ src/test/java/com/ii/app/controllers/BankAccTypeControllerTests.java
@@ -0,0 +1,61 @@
+package com.ii.app.controllers;
+
+import com.ii.app.dto.out.BankAccTypeOut;
+import com.ii.app.models.BankAccType;
+import com.ii.app.models.enums.BankAccountType;
+import com.ii.app.services.interfaces.BankAccTypeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BankAccTypeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BankAccTypeService bankAccTypeService;
+
+    @Before
+    public void setup() {
+        when(bankAccTypeService.findAll()).thenReturn(
+            Arrays.asList(
+                BankAccTypeOut.builder()
+                    .bankAccountType(BankAccountType.MULTI_CURRENCY)
+                    .exchangeCurrencyCommission(2f)
+                    .transactionComission(3f
+                    ).build(),
+                BankAccTypeOut.builder()
+                    .bankAccountType(BankAccountType.STUDENT)
+                    .exchangeCurrencyCommission(2f)
+                    .transactionComission(3f
+                    ).build()
+            )
+        );
+    }
+
+    @Test
+    public void findAllTest() throws Exception {
+        this.mockMvc.perform(get("/api/bankaccounttypes"))
+            .andExpect(status().isOk());
+    }
+}
---
+++ src/test/java/com/ii/app/service/ConversationServiceTest.java
@@ -0,0 +1,28 @@
+package com.ii.app.service;
+
+import com.ii.app.AppApplication;
+import com.ii.app.models.Conversation;
+import com.ii.app.repositories.ConversationRepository;
+import com.ii.app.repositories.UserRepository;
+import com.ii.app.services.interfaces.ConversationService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Instant;
+
+import static org.mockito.Mockito.mock;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@SpringBootTest(classes = AppApplication.class)
+public class ConversationServiceTest {
+
+
+}
