b8946e4bc9543ced1b81e3d2e986cee670546f7f
Grzesiek <grzesiek555_12@outlook.com>
2019-11-18 13:57:42 +0100

    Create tests for email validator

---
+++ src/test/java/com/ii/app/validators/EmailTakenValidatorTest.java
@@ -0,0 +1,44 @@
+package com.ii.app.validators;
+
+import com.ii.app.repositories.UserRepository;
+import com.ii.app.utils.validators.EmailTakenValidatorImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailTakenValidatorTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private EmailTakenValidatorImpl validator;
+
+    @Before
+    public void setup(){
+    }
+
+    @Test
+    public void isValidTest(){
+        when(userRepository.existsByEmail(any(String.class))).thenReturn(false);
+        assertTrue(validator.isValid("test@test.com", null));
+        assertTrue(validator.isValid("test2@test2.com", null));
+    }
+
+    @Test
+    public void isValidTakenEmailTest(){
+        when(userRepository.existsByEmail(any(String.class))).thenReturn(true);
+        assertFalse(validator.isValid("test@test.com", null));
+        assertFalse(validator.isValid(null, null));
+    }
+
+}
