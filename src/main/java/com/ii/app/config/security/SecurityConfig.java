d06630f5eca434ef8db416739c2f10b36c818d92
Grzesiek <grzesiek555_12@outlook.com>
2019-11-18 19:54:57 +0100

    Add port for mobile device

--- src/main/java/com/ii/app/config/security/SecurityConfig.java
+++ src/main/java/com/ii/app/config/security/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:4200", "http://localhost:3333");
 
             }
         };
