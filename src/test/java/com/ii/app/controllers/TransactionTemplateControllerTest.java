6a29d5bc6a07747af6cd9ae154efe7f9acf8c901
Grzesiek <grzesiek555_12@outlook.com>
2019-12-23 15:27:36 +0100

    Create transaction template controller tests

---
+++ src/test/java/com/ii/app/controllers/TransactionTemplateControllerTest.java
@@ -0,0 +1,101 @@
+package com.ii.app.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ii.app.config.security.JwtTokenGenerator;
+import com.ii.app.dto.in.TransactionTemplateIn;
+import com.ii.app.dto.out.TransactionTemplateOut;
+import com.ii.app.services.interfaces.TransactionTemplateService;
+import io.jsonwebtoken.Jwts;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TransactionTemplateControllerTest {
+
+    private final String API_URL = "/api/transactiontemplates";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TransactionTemplateService templateService;
+
+    private static String jwt;
+
+    private final String TEST_ID = "12";
+
+    @BeforeClass
+    public static void setup() {
+        jwt = "Bearer " + JwtTokenGenerator.generate("testUser", "testIdentifier", Collections.singletonList("ROLE_USER"));
+    }
+
+    @Test
+    public void createTest() throws Exception {
+        TransactionTemplateIn templateIn = new TransactionTemplateIn("11112222333344445555666677", "PLN", "11112222333344445555666677", "PLN", BigDecimal.TEN, "Tytul", false, "Nazwa");
+        when(templateService.create(any(TransactionTemplateIn.class))).thenReturn(new TransactionTemplateOut());
+        this.mockMvc.perform(post(API_URL)
+            .header("Authorization", jwt)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(templateIn)))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void findAllTest() throws Exception {
+        when(templateService.findAll()).thenReturn(Arrays.asList(new TransactionTemplateOut(), new TransactionTemplateOut()));
+        this.mockMvc.perform(get(API_URL)
+            .header("Authorization", jwt))
+            .andExpect(jsonPath("$.*", hasSize(2)))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void findOneByIdTest() throws Exception {
+        when(templateService.findOneById(any(Long.class))).thenReturn(new TransactionTemplateOut());
+        this.mockMvc.perform(get(API_URL + "/" + TEST_ID)
+            .header("Authorization", jwt))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateByIdTest() throws Exception {
+        TransactionTemplateIn template = new TransactionTemplateIn("11112222333344445555666677", "PLN", "11112222333344445555666677", "PLN", BigDecimal.TEN, "Tytul", false, "Nazwa");
+        when(templateService.update(any(Long.class), any(TransactionTemplateIn.class))).thenReturn(new TransactionTemplateOut());
+        this.mockMvc.perform(put(API_URL + "/" + TEST_ID)
+            .header("Authorization", jwt)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(template)))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteByIdTest() throws Exception {
+        this.mockMvc.perform(delete(API_URL + "/" + TEST_ID)
+            .header("Authorization", jwt))
+            .andExpect(status().isOk());
+    }
+}
