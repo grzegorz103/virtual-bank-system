ff6404e03d8b165658bd2e3b4143dbb170a7356c
grzegorz103 <grzesiek555_12@outlook.com>
2020-01-20 13:47:12 +0100

    Update UserServiceImpl.java

--- src/main/java/com/ii/app/services/UserServiceImpl.java
+++ src/main/java/com/ii/app/services/UserServiceImpl.java
@@ -77,7 +77,9 @@ public class UserServiceImpl implements UserService {
 
         String identifier = generateIdentifier();
         mapped.setIdentifier(identifier);
-        mapped.setUserRoles(Collections.singleton(userRoleRepository.findByUserType(UserRole.UserType.ROLE_USER)));
+        mapped.setUserRoles(Collections.singleton(
+            userRoleRepository.findByUserType(UserRole.UserType.ROLE_USER))
+        );
         mapped.setPassword(passwordEncoder.encode(userIn.getPassword()));
 
         emailService.sendRegisterMail(mapped.getEmail(), identifier);
